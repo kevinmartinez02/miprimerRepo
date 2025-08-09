bf8d66e3f04669a4f8c63377a59a0e34b1bb4385
Kevin Alfredo Martinez Vasquez <kemart1230@gmail.com>
2025-08-09 15:32:36 -0600

    Probando test automatizado

---
+++ week-5/src/brackets.ts
@@ -0,0 +1,24 @@
+export function balancedBrackets(str:string) {
+    const stack: string[] = []; // Initialize an empty stack
+
+    const openingBrackets = '({[';
+    const closingBrackets = ')}]';
+    const bracketPairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    } as any;
+
+    for (const char of str){
+
+        if (openingBrackets.includes(char)) {
+            stack.push(char);
+        } else if (closingBrackets.includes(char)) {
+            const lastOpening = stack.pop() ;
+            if (!lastOpening || bracketPairs[lastOpening] !== char) {
+                return false; // Mismatched brackets
+            }
+        }
+    }
+    return stack.length === 0; // All opening brackets must be closed
+}
