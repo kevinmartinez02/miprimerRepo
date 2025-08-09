9d6bda00fcc39fa201bf38b0f6fcf6c81ad47c58
Kevin Alfredo Martinez Vasquez <kemart1230@gmail.com>
2025-08-09 15:41:32 -0600

    update: function brackets.ts

--- week-5/src/brackets.ts
+++ week-5/src/brackets.ts
@@ -4,7 +4,7 @@ export function balancedBrackets(str:string) {
     const openingBrackets = '({[';
     const closingBrackets = ')}]';
     const bracketPairs = {
-        '(': ')',
+       // '(': '',
         '{': '}',
         '[': ']'
     } as any;
