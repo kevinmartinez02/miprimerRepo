1834a1893c2da06bbeca8bca9ad0de3a17ea8af5
Kevin Alfredo Martinez Vasquez <kemart1230@gmail.com>
2025-08-24 22:34:33 -0600

    subiendo cambios para probar git actions

--- week-5/test/isMultiple.test.ts
+++ week-5/test/isMultiple.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest';
 import { isMultipleOfFive } from '../src/isMultipleOfFive';
 
 test('if multiple of five,return true',()=>{
-   expect(isMultipleOfFive(10)).toBe(false);
+   expect(isMultipleOfFive(10)).toBe(true);
 })
 
 test('if not multiple of five,return false',()=>{
